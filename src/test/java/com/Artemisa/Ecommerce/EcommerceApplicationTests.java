86c8d3ccd0c25359203dfa40b3834d56bc8f82f9
JCGM <juliancamilo0210@gmail.com>
2024-09-05 22:08:54 -0500

    Initial commit with Spring structure

---
+++ src/test/java/com/Artemisa/Ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Artemisa.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
